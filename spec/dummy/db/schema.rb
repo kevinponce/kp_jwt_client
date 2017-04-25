e2db67f2e50718ebf384ea62c623ee039ce77e0d
Kevin Ponce <kevinponce88@gmail.com>
2017-04-24 18:08:18 -0700

    updated db

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418170118) do
+ActiveRecord::Schema.define(version: 20170417171339) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
