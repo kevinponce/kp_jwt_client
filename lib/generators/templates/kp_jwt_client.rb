1547aa3a80088a38672779a0a3d338850a2eec4a
Kevin Ponce <kevinponce88@gmail.com>
2017-04-24 17:57:09 -0700

    added comment to warn user that token_secret_signature_key mus match parent token_secret_signature_key

--- lib/generators/templates/kp_jwt_client.rb
+++ lib/generators/templates/kp_jwt_client.rb
@@ -3,6 +3,7 @@
 # Use this hook to configure KpJwtClient
 KpJwtClient.setup do |config|
   # Signature key
+  # this token must match parent where the tokens are created
   # by default it is set to -> { Rails.application.secrets.secret_key_base }
   # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
 end
